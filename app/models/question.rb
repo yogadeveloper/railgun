50dca01bf70780434548ea06cc288e12f296ca58
yogadeveloper <shivshankara@yandex.ru>
2016-04-28 12:57:05 +0700

    validate minimum length of Question body

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
+  validates_length_of :body, minimum: 40
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Question do
   it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).is_at_least(40) }
   it { should validate_presence_of(:title) }
   it { should have_many(:answers).dependent(:destroy) }
+
 end
