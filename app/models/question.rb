5c8543a44ac104c9533b6619ea75e44ebfa3bd33
yogadeveloper <shivshankara@yandex.ru>
2016-05-02 21:50:18 +0700

    user can create question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
 
   validates :title, length: { minimum: 5  }, presence: true
-  validates :body,  length: { minimum: 40 }, presence: true
+  validates :body,  length: { minimum: 5 }, presence: true
 end
