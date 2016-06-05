e7e0353f21c0563680e46d568ed3146a30d416d4
yogadeveloper <shivshankara@yandex.ru>
2016-06-05 08:39:51 +0800

    acceptance test for best answer order

--- spec/acceptance/answer/best_answer_spec.rb
+++ spec/acceptance/answer/best_answer_spec.rb
@@ -5,14 +5,16 @@ feature 'Can set the best answer', %q{
 } do
   given!(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given!(:answer) { create_list(:answer, 5, question: question, user: user) }
+  given!(:answer1) { create(:answer, question: question, user: user) }
   given!(:non_author) { create(:user) }
-  given!(:answer_to_mark) { create(:answer, body: 'I am the best answer and I am on the top', question: question, user: user) }
+  given!(:answer2) { create(:answer, body: 'I am the best answer and I am on the top',
+                                                    question: question, user: user) }
 
   scenario 'Non-authenticated user tries to set the best answer', js: true do
     visit question_path(question)
     expect(page).to_not have_content "Mark as Best"
   end
+
   describe 'Authenticated user' do
     before do
       sign_in(user)
@@ -24,7 +26,7 @@ feature 'Can set the best answer', %q{
     end
 
     scenario "try to mark best answer", js: true do
-      within("div#answer-#{answer_to_mark.id}") do
+      within("div#answer-#{answer2.id}") do
         expect(page).to_not have_selector("best")
         click_on "Mark as Best"
       end
@@ -32,6 +34,14 @@ feature 'Can set the best answer', %q{
         expect(page).to have_content 'nice!'
         expect(page).to have_content 'I am the best answer and I am on the top'
       end
+    end
+
+    scenario "The best answer on the top", js: true do
+      within("div#answer-#{answer2.id}") do
+        expect(page).to_not have_selector("best")
+          click_on "Mark as Best"
+      end
+      expect(page.find('.answer:nth-of-type(1)')).to have_content 'I am the best answer and I am on the top'
     end
   end
 
