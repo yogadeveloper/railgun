dac17a9a4d00777464c5a5f9de98db98e84a6628
yogadeveloper <shivshankara@yandex.ru>
2016-06-06 10:32:12 +0800

    Question accepts nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,6 @@ class Question < ActiveRecord::Base
   validates :title, length: { minimum: 5  }, presence: true
   validates :body,  length: { minimum: 5 }, presence: true
   validates :user_id, presence: true
+
+  accepts_nested_attributes_for :attachments
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,12 +3,17 @@
 require 'rails_helper'
 
 describe Question do
+  it { should belong_to(:user) }
+
   it { should validate_presence_of(:body) }
   it { should validate_length_of(:body).is_at_least(5) }
   it { should validate_presence_of(:title) }
   it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_presence_of(:user_id) }
+
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_many :attachments }
-  it { should belong_to(:user) }
-  it { should validate_presence_of(:user_id) }
+
+  it {should accept_nested_attributes_for :attachments }
+
 end
