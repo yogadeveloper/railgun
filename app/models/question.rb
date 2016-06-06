29e2ddf796038be7f9bc0ee8b1808e9b7467e4b2
yogadeveloper <shivshankara@yandex.ru>
2016-06-06 12:12:11 +0800

    Polymorphic attachments. Answers associated with attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :attachments
+  has_many :attachments, as: :attachable
 
   validates :title, length: { minimum: 5  }, presence: true
   validates :body,  length: { minimum: 5 }, presence: true
