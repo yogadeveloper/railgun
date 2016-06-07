f16bcf1c5a496d7d03241e07086761d5c940f136
yogadeveloper <shivshankara@yandex.ru>
2016-06-07 11:19:48 +0800

    attachments models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
-  has_many :attachments, as: :attachable
+  has_many :attachments, dependent: :destroy, as: :attachable
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   default_scope { order(best: :desc) }
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,11 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :attachments, as: :attachable
+  has_many :attachments, dependent: :destroy, as: :attachable
 
   validates :title, length: { minimum: 5  }, presence: true
   validates :body,  length: { minimum: 5 }, presence: true
   validates :user_id, presence: true
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 end
