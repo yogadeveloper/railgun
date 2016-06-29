92bd0636ac5d64d26ef36432e50084cb8a822cda
yogadeveloper <shivshankara@yandex.ru>
2016-06-29 09:10:54 +0800

    fix profiles controller

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ProfilesController < ApplicationController
   skip_authorization_check
 
   before_action :doorkeeper_authorize!
-  before_action :except_current_user, only: :index
 
   respond_to :json
 
@@ -11,14 +10,11 @@ class Api::V1::ProfilesController < ApplicationController
   end
 
   def index
+    @profiles = User.where.not(id: current_resource_owner.id)
     respond_with @profiles
   end
 
   protected
-
-  def except_current_user
-    @profiles = User.where.not(id: current_resource_owner.id)
-  end
 
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
