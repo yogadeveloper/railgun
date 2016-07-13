15c0006b0ae3c241c4894f2225face83325e4fff
yogadeveloper <shivshankara@yandex.ru>
2016-07-14 07:38:57 +0800

    fix authorizations migration for deploy

--- db/migrate/20160617095646_remove_foreign_key_from_authorizatinos.rb
+++ db/migrate/20160617095646_remove_foreign_key_from_authorizatinos.rb
@@ -1,6 +1,5 @@
 class RemoveForeignKeyFromAuthorizatinos < ActiveRecord::Migration
   def change
-    remove_foreign_key :authorizations, :users
-
+#    remove_foreign_key :authorizations, :users
   end
 end
